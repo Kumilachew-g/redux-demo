3317803f6cfda51df68e398d09dd97f54541ea17
Kumilachew-g <kumilachew1212@gmail.com>
2022-09-20 01:38:21 +0300

    Some changes

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
-const redux = require('redux')
-const createStore =redux.createStore
+const redux = require('redux');
+const createStore = redux.createStore;
 
 const cakeOrdered = 'cakeOrdered';
 
-
 function orderCake() {
-return {
-    type : cakeOrdered,
+  return {
+    type: cakeOrdered,
     quantity: 1,
-  }
+  };
 }
 
 const initialState = {
-    numOfCakes :10,
-}
+  numOfCakes: 10,
+};
 
 const reducer = (state = initialState, action) => {
-    switch(action.type){
-        case cakeOrdered:
-            return{
-                ...state,
-                numOfCakes: state.numOfCakes -1,
-            }
-            default:
-                return state
-    }
-}
-const store = createStore(reducer)
-console.log('Initial State', store.getState())
-const unsubscribe = store.subscribe(() => console.log('update state', store.getState()))
-store.dispatch(orderCake())
-store.dispatch(orderCake())
-store.dispatch(orderCake())
+  switch (action.type) {
+    case cakeOrdered:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+const store = createStore(reducer);
+console.log('Initial State', store.getState());
+const unsubscribe = store.subscribe(() =>
+  console.log('update state', store.getState())
+);
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
 
-unsubscribe()
+unsubscribe();
